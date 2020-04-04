df23ec7f41ce42f47ebd605a58b98796b985c07b
mitant127 <55878970+mitant127@users.noreply.github.com>
2020-04-04 12:20:35 +0300

    HW_4
    Четвертое домашнее задание.

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -12,3 +12,230 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+
+import timeit
+from random import randint
+import cProfile
+
+
+def hw3_tsk2_v1():
+    MY_LIST = [randint(-100, 100) for i in range(1000)]
+    # print(MY_LIST)
+    x1 = list(i for i in MY_LIST if i % 2 == 0)
+
+
+def hw3_tsk2_v2():
+    MY_LIST = [randint(-100, 100) for i in range(1000)]
+    # print(MY_LIST)
+    x2 = []
+    for i in MY_LIST:
+        if i % 2 == 0:
+            x2.append(i)
+
+
+def main():
+    hw3_tsk2_v1()
+    hw3_tsk2_v2()
+
+
+print(timeit.timeit("hw3_tsk2_v1()", setup="from __main__ import hw3_tsk2_v1", number=10000))
+print(timeit.timeit("hw3_tsk2_v2()", setup="from __main__ import hw3_tsk2_v2", number=10000))
+
+
+cProfile.run("main()")
+
+
+"""Попробовал ухудшить алгоритм. При использовании цикла вместо генератора наблюдается незначительное 
+увеличение времение выполнения. Но значительное время все равно занимает генератор заполнения массива MY_LIST"""
+
+
+"""NUMB = 932850971070143275070239750527932850971070143275070239750527
+
+
+def recursion(numb, even, uneven):
+    # mathematical character
+    if numb < 10:
+        if numb % 2 == 0:
+            even += 1
+        else:
+            uneven += 1
+        # print(f'Четные {even}   Нечетные {uneven}')
+        return 0
+    else:
+        if (numb % 10) % 2 == 0:
+            even += 1
+        else:
+            uneven += 1
+        return recursion(numb // 10, even, uneven)
+
+
+def funct():
+    NUMB = 932850971070143275070239750527932850971070143275070239750527
+    EVEN = 0
+    UNEVEN = 0
+
+    while NUMB > 9:
+        if (NUMB % 10) % 2 == 0:
+            EVEN += 1
+        else:
+            UNEVEN += 1
+
+        NUMB = NUMB // 10
+
+    if NUMB % 2 == 0:
+        EVEN += 1
+    else:
+        UNEVEN += 1
+
+    # print(f'Четные {EVEN}   Нечетные {UNEVEN}')
+
+
+def main():
+    recursion(NUMB, 0, 0)
+    funct()
+
+
+cProfile.run('main()')"""
+
+"""NUMB = 35982093509283092039850932
+
+
+def recursion(numb, inverse_numb):
+    # mathematical character
+    if numb == 0:
+        print(f'Перевернутое число: {inverse_numb}')
+        return 0
+    else:
+        excess = numb % 10  # находим остаток - последнюю цифру числа
+        inverse_numb = inverse_numb * 10  # увеличиваем разрядность второго числа
+        return recursion(numb // 10, inverse_numb + excess)
+
+
+def funct(NUMB):
+    INVERSE_NUMB = 0
+    while NUMB > 0:
+        excess = NUMB % 10  # находим остаток - последнюю цифру числа
+        NUMB = NUMB // 10  # делим нацело - убираем из числа последнюю цифру
+        INVERSE_NUMB = INVERSE_NUMB * 10  # увеличиваем разрядность второго числа
+        INVERSE_NUMB = INVERSE_NUMB + excess  # добавляем очередную цифру
+
+
+def main():
+    recursion(NUMB, 0)
+    funct(NUMB)
+
+
+cProfile.run('main()')"""
+
+
+def hw3_tsk4_v1():
+    MY_LIST = [randint(0, 2) for i in range(10)]
+    MY_DICT = dict()
+    for i in MY_LIST:
+        if MY_DICT.get(i) is None:
+            MY_DICT.update({i: 1})
+        else:
+            MY_DICT[i] += 1
+
+    x1 = max(MY_DICT, key=MY_DICT.get)
+
+
+def hw3_tsk1_v1():
+    MY_DIKT = {a: 0 for a in range(2, 10)}  # создаем словарь с ключами 2 - 9 и значением 0
+    # print(MY_DIKT)
+
+    for i in range(2, 100):  # перебор значений 2 - 99
+        for j in range(2, 10):  # проверка каждого числа на кратность каждому ключу словаря
+            if i % j == 0:
+                MY_DIKT[j] += 1
+
+
+def hw3_tsk5_v1():
+    MY_LIST = [randint(-100, 100) for i in range(10)]
+    # print(f'Базовый список: {MY_LIST}')
+
+    MAX_NEGATIVE = min(MY_LIST)
+    for i in MY_LIST:
+        if MAX_NEGATIVE < i < 0:
+            MAX_NEGATIVE = i
+
+
+def hw3_tsk6_v1():
+    MY_LIST = [randint(0, 100) for i in range(10)]
+    # print(f'Исходный список {MY_LIST}')
+
+    INDX_1 = max(MY_LIST)
+    INDX_2 = min(MY_LIST)
+    SUMM = 0
+    # print(f'{MY_LIST.index(INDX_1)}, {MY_LIST.index(INDX_2)}')
+    if MY_LIST.index(INDX_1) > MY_LIST.index(INDX_2):
+        for i in range(MY_LIST.index(INDX_2) + 1, MY_LIST.index(INDX_1)):
+            SUMM += MY_LIST[i]
+    else:
+        for i in range(MY_LIST.index(INDX_1) + 1, MY_LIST.index(INDX_2)):
+            SUMM += MY_LIST[i]
+
+
+def hw3_tsk7_v1():
+    MY_LIST = [randint(-10000, 10000) for i in range(100000)]
+    # print(f'Исходный массив: {MY_LIST}')
+    MINN = None
+
+    for i in range(2):
+        if i > 0:
+            if MINN != min(MY_LIST):
+                MINN = min(MY_LIST)
+                # print(f'Второй наименьший элемент: {MINN}')
+                MY_LIST.remove(MINN)
+            # else:
+                # print(f'Встречается в этом массиве 2 раз')
+        else:
+            MINN = min(MY_LIST)
+            # print(f'Наименьший элемент: {MINN}')
+            MY_LIST.remove(MINN)
+
+
+def hw3_tsk7_v2():
+    MY_LIST = [randint(-10000, 10000) for i in range(100000)]
+    # print(f'Исходный массив: {MY_LIST}')
+    MINN = None
+
+    for i in range(2):
+        if i > 0:
+            if MINN != min(MY_LIST):
+                MINN = min(MY_LIST)
+                # print(f'Второй наименьший элемент: {MINN}')
+                MY_LIST.remove(MINN)
+            # else:
+                # print(f'Встречается в этом массиве 2 раз')
+        else:
+            MINN = min(MY_LIST)
+            # print(f'Наименьший элемент: {MINN}')
+            MY_LIST.remove(MINN)
+
+
+def hw3_tsk8_v1():
+    MY_LIST = [[randint(-1000, 1000) for i in range(1000)] for j in range(1000)]
+    # print(MY_LIST)
+
+    # for i in range(5):
+    #     print(f'{i + 1}-я строка: ')
+    #     MY_LIST.append([])
+    #     for j in range(4):
+    #         MY_LIST[i].append(int(input('')))
+
+    for k in range(len(MY_LIST)):
+        MY_LIST[k].append(sum(MY_LIST[k]))
+        # print(MY_LIST[k])
+
+
+# def main():
+#     hw3_tsk7_v1()
+#
+#
+# cProfile.run('main()')
+
+
+"""cProfile везде показывает по 0.000. При увеличении исходных массивов может наблюдаться 
+увеличение времени, но она касается только создания самого массива, а алгоритм его обработки 
+ так и показывает нули."""
--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -12,3 +12,280 @@
 ВНИМАНИЕ: ЗАДАНИЯ, В КОТОРЫХ БУДУТ ГОЛЫЕ ЦИФРЫ ЗАМЕРОВ (БЕЗ АНАЛИТИКИ)
 БУДУТ ПРИНИМАТЬСЯ С ОЦЕНКОЙ УДОВЛЕТВОРИТЕЛЬНО
 """
+
+"""При написании программы нашел статью https://habr.com/ru/post/122538/
+в ней рассматривается постепенное доработка алгоритма.
+Тк написанная мной программа была похожа на 3-ю версию программы из статьи
+я решил не изобретать велосипед, а проверить все версии приведенных в статье программ."""
+
+
+from timeit import Timer
+from math import sqrt, log1p
+import matplotlib.pyplot as plt
+G2 = [100, 200, 400, 600, 800, 1000]
+G1 = [10, 20, 40, 60, 80, 100]
+Z = 5  # количество циклов для замера времени
+# N = 10  # искомое простое число
+Z1, Z2, Z3, Z4, Z5, Z6, Z7, Z8, Z4_2, Z5_2, Z6_2, Z7_2, Z8_2 = [
+], [], [], [], [], [], [], [], [], [], [], [], []
+# массивы для заполнения времени
+
+"""Первый алгоритм с перебором всех чисел и всех делителей и подсчетом найденых делителей
+Занимает много времени тк мы перебираем все числа и все делители. """
+
+
+def srch_prm_num_1(n):
+    prime_num = []
+    k = 0
+    i = 1
+    while n > int(len(prime_num)):
+        i += 1
+        for j in range(2, i):
+            if i % j == 0:
+                k = k + 1
+        if k == 0:
+            prime_num.append(i)
+        else:
+            k = 0
+    # print(prime_num)
+
+
+""""Избавляемся от подсчета делителей тк если найден хотябы один то число составное
+и дальше делители можно не перебирать."""
+
+
+def srch_prm_num_2(n):
+    prime_num = []
+    i = 1
+    while n > int(len(prime_num)):
+        i += 1
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            prime_num.append(i)
+    # print(prime_num)
+
+
+"""Тут делаем перебор делителей только по уже найденным простым числам"""
+
+
+def srch_prm_num_3(n):
+    prime_num = []
+    i = 1
+    while n > int(len(prime_num)):
+        i += 1
+        for j in prime_num:
+            if i % j == 0:
+                break
+        else:
+            prime_num.append(i)
+    # print(prime_num)
+
+
+"""Тут добавляем проверку на то, что делитель не превышает корня из числа для котороно ищем делитель"""
+
+
+def srch_prm_num_4(n):
+    prime_num = []
+    i = 1
+    while n > int(len(prime_num)):
+        i += 1
+        for j in prime_num:
+            if j > int((sqrt(i)) + 1):
+                prime_num.append(i)
+                break
+            if i % j == 0:
+                break
+        else:
+            prime_num.append(i)
+    # print(prime_num)
+
+
+"""Тут добавляем отсеивание чисела кратные 2 и 5"""
+
+
+def srch_prm_num_5(n):
+    prime_num = []
+    i = 1
+    while n > int(len(prime_num)):
+        i += 1
+        if i > 10:
+            if i % 2 == 0 or i % 10 == 5:
+                continue
+        for j in prime_num:
+            if j > int((sqrt(i)) + 1):
+                prime_num.append(i)
+                break
+            if i % j == 0:
+                break
+        else:
+            prime_num.append(i)
+    # print(prime_num)
+
+
+"""Тут перебираем делители с шагом 2 что позволяет сразу отсеять четные числа."""
+
+
+def srch_prm_num_6(n):
+    prime_num = [2]
+    i = 1
+    while n > int(len(prime_num)):
+        i += 2
+        if i > 10 and i % 10 == 5:
+            continue
+        for j in prime_num:
+            if j > int((sqrt(i)) + 1):
+                prime_num.append(i)
+                break
+            if i % j == 0:
+                break
+        else:
+            prime_num.append(i)
+    # print(prime_num)
+
+
+"""Заменяем извлечение корня на возведение в степень"""
+
+
+def srch_prm_num_7(n):
+    prime_num = [2]
+    i = 1
+    while n > int(len(prime_num)):
+        i += 2
+        if i > 10 and i % 10 == 5:
+            continue
+        for j in prime_num:   # деление очередного числа на уже найденные простые числа
+            if j * j - 1 > i:
+                prime_num.append(i)
+                break  # если делитель найден выходим из цикла
+            if i % j == 0:  # проверка делимости очередного числа на элементы массива
+                break
+        else:
+            # если делитель не найден добавляем число в массив простых чисел
+            prime_num.append(i)
+    # print(prime_num[int(len(prime_num)) - 1])
+
+
+def eratosfen(n):
+    p = int(n * log1p(2 * n)) + n
+    prime_num = []
+    a = [i for i in range(p)]
+    i = 2
+    while i <= p - 1:
+        if a[i] != 0:
+            prime_num.append(a[i])
+            if n <= int(len(prime_num)):
+                break
+            for j in range(i, p, i):
+                a[j] = 0
+        i += 1
+    # print(prime_num[int(len(prime_num)) - 1])
+
+
+for elm in G1:
+    print(G1.index(elm))
+    t1 = Timer(
+        "srch_prm_num_1(elm)",
+        "from __main__ import srch_prm_num_1, elm")
+    # print("concat 1", t1.timeit(number=Z), "miliseconds")
+    Z1.append(t1.timeit(number=Z))
+    t2 = Timer(
+        "srch_prm_num_2(elm)",
+        "from __main__ import srch_prm_num_2, elm")
+    # print("concat 2", t2.timeit(number=Z), "miliseconds")
+    Z2.append(t2.timeit(number=Z))
+    t3 = Timer(
+        "srch_prm_num_3(elm)",
+        "from __main__ import srch_prm_num_3, elm")
+    # print("concat 3", t3.timeit(number=Z), "miliseconds")
+    Z3.append(t3.timeit(number=Z))
+    t4 = Timer(
+        "srch_prm_num_4(elm)",
+        "from __main__ import srch_prm_num_4, elm")
+    # print("concat 4", t4.timeit(number=Z), "miliseconds")
+    Z4.append(t4.timeit(number=Z))
+    t5 = Timer(
+        "srch_prm_num_5(elm)",
+        "from __main__ import srch_prm_num_5, elm")
+    # print("concat 5", t5.timeit(number=Z), "miliseconds")
+    Z5.append(t5.timeit(number=Z))
+    t6 = Timer(
+        "srch_prm_num_6(elm)",
+        "from __main__ import srch_prm_num_6, elm")
+    # print("concat 6", t6.timeit(number=Z) , "miliseconds")
+    Z6.append(t6.timeit(number=Z))
+    t7 = Timer(
+        "srch_prm_num_7(elm)",
+        "from __main__ import srch_prm_num_7, elm")
+    # print("concat 7", t7.timeit(number=Z), "miliseconds")
+    Z7.append(t7.timeit(number=Z))
+    t8 = Timer("eratosfen(elm)", "from __main__ import eratosfen, elm")
+    # print("concat 8", t8.timeit(number=Z), "miliseconds")
+    Z8.append(t8.timeit(number=Z))
+
+for el in G2:
+    print(G2.index(el))
+    t4 = Timer("srch_prm_num_4(el)", "from __main__ import srch_prm_num_4, el")
+    # print("concat 4", t4.timeit(number=Z), "miliseconds")
+    Z4_2.append(t4.timeit(number=Z))
+    t5 = Timer("srch_prm_num_5(el)", "from __main__ import srch_prm_num_5, el")
+    # print("concat 5", t5.timeit(number=Z), "miliseconds")
+    Z5_2.append(t5.timeit(number=Z))
+    t6 = Timer("srch_prm_num_6(el)", "from __main__ import srch_prm_num_6, el")
+    # print("concat 6", t6.timeit(number=Z) , "miliseconds")
+    Z6_2.append(t6.timeit(number=Z))
+    t7 = Timer("srch_prm_num_7(el)", "from __main__ import srch_prm_num_7, el")
+    # print("concat 7", t7.timeit(number=Z), "miliseconds")
+    Z7_2.append(t7.timeit(number=Z))
+    t8 = Timer("eratosfen(el)", "from __main__ import eratosfen, el")
+    # print("concat 8", t8.timeit(number=Z), "miliseconds")
+    Z8_2.append(t8.timeit(number=Z))
+
+print(f'{Z1}\n{Z2}\n{Z3}\n{Z4}\n{Z5}\n{Z6}\n{Z7}\n{Z8}\n{Z4_2}\n{Z5_2}\n{Z6_2}\n{Z7_2}\n{Z8_2}')
+
+# Построение графика
+
+plt.figure(figsize=(15, 10))
+
+plt.subplot(2, 1, 1)
+plt.title("Зависимости: времени от количества простых чисел")  # заголовок
+plt.xlabel("кол-во прост. чисел")         # ось абсцисс
+plt.ylabel("время млс")    # ось ординат
+plt.grid()              # включение отображение сетки
+plt.plot(G1, Z1, c="brown", label="1")
+plt.plot(G1, Z2, c="blue", label="2")
+plt.plot(G1, Z3, c="yellow", label="3")
+plt.plot(G1, Z4, c="black", label="4")
+plt.plot(G1, Z5, c="green", label="5")
+plt.plot(G1, Z6, c="orange", label="6")
+plt.plot(G1, Z7, c="gray", label="7")
+plt.plot(G1, Z8, c="red", label="8")
+plt.legend()
+# plt.show()
+
+# Построение графика
+plt.subplot(2, 1, 2)
+plt.title("Зависимости: времени от количества простых чисел")  # заголовок
+plt.xlabel("кол-во прост. чисел")         # ось абсцисс
+plt.ylabel("время млс")    # ось ординат
+plt.grid()              # включение отображение сетки
+plt.plot(G2, Z4_2, c="black", label="4")
+plt.plot(G2, Z5_2, c="green", label="5")
+plt.plot(G2, Z6_2, c="orange", label="6")
+plt.plot(G2, Z7_2, c="gray", label="7")
+plt.plot(G2, Z8_2, c="red", label="8")
+plt.legend()
+plt.show()
+
+
+"""Выводы:
+Как и ожидалось первый алгоритм с перебором всех чисел по порядку и всех делителей
+ дает худшие результаты тк получается квадратичная сложность O(N^2)
+
+ На малых значениях N хорошо себя показывае алгоритм 3 и 7
+ Алгоритмы 4, 5, 6 скорее всего "проседают" по скорости из-за использования
+ функции sqrt(i)
+
+На больших значениях хорошо работает алгоритм 7 и решето Эратосфена, оно показывает себя лучше всего
+сложность алгоритма O(N)"""
